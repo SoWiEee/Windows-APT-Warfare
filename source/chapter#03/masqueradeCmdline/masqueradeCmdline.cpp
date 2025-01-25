5be6fd7a76d6351748703d8ed8dff4295db50655
SoWiEee <99129463+SoWiEee@users.noreply.github.com>
2025-01-25 11:11:13 +0800

    Update masqueradeCmdline.cpp

--- source/chapter#03/masqueradeCmdline/masqueradeCmdline.cpp
+++ source/chapter#03/masqueradeCmdline/masqueradeCmdline.cpp
@@ -14,7 +14,7 @@
 
 int main(void) {
 	PROCESS_INFORMATION PI = {}; STARTUPINFOA SI = {}; CONTEXT CTX = { CONTEXT_FULL };
-	RTL_USER_PROCESS_PARAMETERS parentParamIn;
+	RTL_USER_PROCESS_PARAMETERS parentParamIn;	// 包含 child 被生成當下的參數資訊
 	PEB remotePeb;
 
 	char dummyCmdline[MAX_PATH]; /* AAA... 260 bytes */
@@ -22,13 +22,12 @@ int main(void) {
 	memset(dummyCmdline, 'A', sizeof(dummyCmdline));
 
 	wchar_t new_szCmdline[] = L"/c whoami & echo P1ay Win32 L!k3 a K!ng. & sleep 100";
-	// 用 CreateProcess 將 Windows 自帶的 32bitcmd.exe 建立為 Thread 暫停狀態
-	// 將剛剛生成的垃圾參數當作 cmd.exe 被傳入的參數
+	// 將 Windows 自帶的 32bitcmd.exe 建立為 Thread (SUSPENDED)，並傳入垃圾參數
 	CreateProcessA("C:/Windows/SysWOW64/cmd.exe", dummyCmdline, 0, 0, 0, CREATE_SUSPENDED, 0, 0, &SI, &PI);
 	// 用 GetThreadContext 取得當下暫停住 Theard 之暫存器內容
 	GetThreadContext(PI.hThread, &CTX);
 
-	// 用 ReadProcessMemory 將 child Process 的 PEB 內容取出
+	// 將 child Process 的 PEB 內容取出
 	ReadProcessMemory(PI.hProcess, LPVOID(CTX.Ebx), &remotePeb, sizeof(remotePeb), 0);
 
 	// 在 PEB 中 ProcessParameters 欄位取得當前 child process 的 RTL_USER_PROCESS_PARAMETERS 結構的位址
@@ -40,7 +39,6 @@ int main(void) {
 	// 以 WriteProcessMemory 將想執行的文字參數覆寫上去
 	WriteProcessMemory(PI.hProcess, parentParamIn.CommandLine.Buffer, new_szCmdline, sizeof(new_szCmdline), 0);
 
-	// resume main thread of the child process.
 	// 恢復 Thread 運作
 	ResumeThread(PI.hThread);
 	return 0;
