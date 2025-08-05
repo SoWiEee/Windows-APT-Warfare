1969644b2eabf93262877eb8ed3a135e6d5517b5
SoWiEee <99129463+SoWiEee@users.noreply.github.com>
2025-08-05 19:11:01 +0800

    Update iatHook.cpp

--- source/chapter#05/iatHook.cpp
+++ source/chapter#05/iatHook.cpp
@@ -1,11 +1,7 @@
-/**
- * iatHook.cpp
- * Windows APT Warfare
- * by aaaddress1@chroot.org
- */
 #include <stdio.h>
 #include <windows.h>
 #pragma warning(disable : 4996)
+// get NT Headers, Sections Header
 #define getNtHdr(buf) ((IMAGE_NT_HEADERS *)((size_t)buf + ((IMAGE_DOS_HEADER *)buf)->e_lfanew))
 #define getSectionArr(buf) ((IMAGE_SECTION_HEADER *)((size_t)buf + ((IMAGE_DOS_HEADER *)buf)->e_lfanew + sizeof(IMAGE_NT_HEADERS)))
 
@@ -13,19 +9,21 @@ size_t ptr_msgboxa = 0;
 
 void iatHook(char *module, const char *szHook_ApiName, size_t callback, size_t &apiAddr)
 {
-	auto dir_ImportTable = getNtHdr(module)->OptionalHeader.DataDirectory[IMAGE_DIRECTORY_ENTRY_IMPORT];
-	auto impModuleList = (IMAGE_IMPORT_DESCRIPTOR *)&module[dir_ImportTable.VirtualAddress];
+	auto dir_ImportTable = getNtHdr(module)->OptionalHeader.DataDirectory[IMAGE_DIRECTORY_ENTRY_IMPORT];	// IAT
+	auto impModuleList = (IMAGE_IMPORT_DESCRIPTOR *)&module[dir_ImportTable.VirtualAddress];		// pointer to IMAGE_IMPORT_DESCRIPTOR
+	// traversal all DLLs until null DESCRIPTOR
 	for (; impModuleList->Name; impModuleList++)
 	{
-		auto arr_callVia = (IMAGE_THUNK_DATA *)&module[impModuleList->FirstThunk];
-		auto arr_apiNames = (IMAGE_THUNK_DATA *)&module[impModuleList->OriginalFirstThunk];
+		auto arr_callVia = (IMAGE_THUNK_DATA *)&module[impModuleList->FirstThunk];			// pointer to IAT
+		auto arr_apiNames = (IMAGE_THUNK_DATA *)&module[impModuleList->OriginalFirstThunk];		// pointer to INT
 		for (int i = 0; arr_apiNames[i].u1.Function; i++)
 		{
-			auto curr_impApi = (PIMAGE_IMPORT_BY_NAME)&module[arr_apiNames[i].u1.Function];
-			if (!strcmp(szHook_ApiName, (char *)curr_impApi->Name))
+			auto curr_impApi = (PIMAGE_IMPORT_BY_NAME)&module[arr_apiNames[i].u1.Function];		// current API name
+			
+			if (!strcmp(szHook_ApiName, (char *)curr_impApi->Name))					// compare wanted API name
 			{
-				apiAddr = arr_callVia[i].u1.Function;
-				arr_callVia[i].u1.Function = callback;
+				apiAddr = arr_callVia[i].u1.Function;						// save MessageBoxA's address
+				arr_callVia[i].u1.Function = callback;						// replace with callback
 				break;
 			}
 		}
